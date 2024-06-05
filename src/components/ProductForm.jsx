c4743ece50440af9f51b223b38513d35aaca0911
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-06-05 16:45:03 +0700

    feat: alert form data

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,52 +1,96 @@
+import { useState } from "react";
+
 function ProductForm() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let newFormData = {
+      name: name,
+      image: image,
+      price: price,
+      description: description,
+    };
+
+    alert(JSON.stringify(newFormData, null, 2));
+
+    setName("");
+    setImage("");
+    setPrice("");
+    setDescription("");
+  }
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleImageChange = (event) => {
+    setImage(event.target.value);
+  };
+
+  const handlePriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSubmit}>
       <h1>Create Product Form</h1>
       <div className="input-container">
-        <label>
+        <label htmlFor="name">
           Name
           <input
             id="name"
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            onChange={handleNameChange}
+            value={name}
           />
         </label>
       </div>
       <div className="input-container">
-        <label>
+        <label htmlFor="image">
           Image Url
           <input
             id="image"
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            onChange={handleImageChange}
+            value={image}
           />
         </label>
       </div>
       <div className="input-container">
-        <label>
+        <label htmlFor="price">
           Price
           <input
             id="price"
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            onChange={handlePriceChange}
+            value={price}
           />
         </label>
       </div>
       <div className="input-container">
-        <label>
+        <label htmlFor="description">
           Description
           <textarea
             id="description"
             name="description"
-            type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            onChange={handleDescriptionChange}
+            value={description}
             rows={4}
             cols={30}
           />
